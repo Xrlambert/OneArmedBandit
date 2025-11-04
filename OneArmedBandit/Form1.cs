da9c0a9b1b141070c4da29acb6c886727840b1df
X Lamb <xavierlambertt@gmail.com>
2025-11-04 10:53:45 -0500

    Doneso

--- OneArmedBandit/Form1.cs
+++ OneArmedBandit/Form1.cs
@@ -1,128 +1,122 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System;
 using System.Windows.Forms;
-using System.Media;
 using System.Threading;
-
 
 namespace OneArmedBandit
 {
     public partial class Form1 : Form
     {
-        // random number generator
-        Random randGen = new Random();
+        // Random number generator
+        private Random rand = new Random();
 
         // int value for score initialized to 10
-        int score = 10;
+        private int score = 10;
 
         public Form1()
         {
             InitializeComponent();
+
+            // display initial score
+            scoreDisplay.Text = score.ToString();
         }
 
-        private void spinButton_Click(object sender, EventArgs e) 
+        private void spinButton_Click(object sender, EventArgs e)
         {
-            // get random values for each reel (store each in separate int variable)
-            int generator1;
-            int generator2;
-            int generator3;
+            // disable button during spin animation
+            spinButton.Enabled = false;
 
-           generator1 = randGen.Next(1, 4);
-           generator2 = randGen.Next(1, 4);
-           generator3 = randGen.Next(1, 4);
+            /// SPIN EFFECT ----------------------------------------------------------
+            /// Use a while loop to simulate spinning reels with semi-exponential delay
+            /// -----------------------------------------------------------------------
+            int delay = 50; // initial delay in milliseconds
+            int maxDelay = 700; // maximum delay before stopping
 
-            // 1 = seven
-            // 2 = cherry
-            // 3 = diamond
-
-
-            // check value of reel 1 with a switch statement to set appropriate image to
-            // BackgroundImage property of pictureBox. Get images from Properties.Resources
-            switch (generator1)
+            while (delay <= maxDelay)
             {
-                case 1:
-                    reel1Output.BackgroundImage = Properties.Resources._7_100x125;
-                    break;
-                case 2:
-                    reel1Output.BackgroundImage = Properties.Resources.cherry_100x125;
-                    break;
-                case 3:
-                    reel1Output.BackgroundImage = Properties.Resources.diamond_100x125;
-                    break;    
+                // get temporary random values for each reel
+                int temp1 = rand.Next(1, 4);
+                int temp2 = rand.Next(1, 4);
+                int temp3 = rand.Next(1, 4);
+
+                // set temporary images to simulate spinning
+                SetReelImage(pictureBox1, temp1);
+                SetReelImage(pictureBox2, temp2);
+                SetReelImage(pictureBox3, temp3);
+
+                // allow UI to update during loop
+                Application.DoEvents();
+
+                // pause for current delay
+                Thread.Sleep(delay);
+
+                // increase delay exponentially to simulate slowing down
+                delay = (int)(delay * 1.3);
             }
 
+            /// FINAL RESULT ----------------------------------------------------------
+            /// Get final random values for each reel and set images accordingly
+            /// -----------------------------------------------------------------------
+            int reel1 = rand.Next(1, 4);
+            int reel2 = rand.Next(1, 4);
+            int reel3 = rand.Next(1, 4);
 
-            // check value of reel 2 with a switch statement to set appropriate image to
-            // BackgroundImage property of pictureBox. Get images from Properties.Resources
-            switch (generator2)
-            {
-                case 1:
-                    reel2Output.BackgroundImage = Properties.Resources._7_100x125;
-                    break;
-                case 2:
-                    reel2Output.BackgroundImage = Properties.Resources.cherry_100x125;
-                    break;
-                case 3:
-                    reel2Output.BackgroundImage = Properties.Resources.diamond_100x125;
-                    break;
-            }
+            SetReelImage(pictureBox1, reel1);
+            SetReelImage(pictureBox2, reel2);
+            SetReelImage(pictureBox3, reel3);
 
-            // check value of reel 3 with a switch statement to set appropriate image to
-            // BackgroundImage property of pictureBox. Get images from Properties.Resources
-            switch (generator3)
-            {
-                case 1:
-                    reel3Output.BackgroundImage = Properties.Resources._7_100x125;
-                    break;
-                case 2:
-                    reel3Output.BackgroundImage = Properties.Resources.cherry_100x125;
-                    break;
-                case 3:
-                    reel3Output.BackgroundImage = Properties.Resources.diamond_100x125;
-                    break;
-            }
-
-            /// STOP HERE ----------------------------------------------------------
-            /// Test to make sure that your program will display random
-            /// images to each reel. Only continue on after you know this works
-            /// --------------------------------------------------------------------
-
-
-            // Use compound if statement to check if all reels are equal. 
-            // If yes show "winner" statement and add 3 to score.
-            // If no show "play again" statement and subtract 1 from score.
-            // 
-
-            if (generator1 == generator2 && generator1 == generator3)
+            /// WIN/LOSS LOGIC --------------------------------------------------------
+            /// Use compound if statement to check if all reels are equal.
+            /// If yes, show "winner" message and add 3 to score.
+            /// If no, show "play again" message and subtract 1 from score.
+            /// -----------------------------------------------------------------------
+            if (reel1 == reel2 && reel2 == reel3)
             {
                 outputLabel.Text = "Winner!";
-                score = score + 3;
-                scoreDisplay.Text = $"{score}";
+                score += 3;
             }
-            else 
+            else
             {
-                outputLabel.Text = "Try Again!";
-                score--;
-                scoreDisplay.Text = $"{score}";
-
+                outputLabel.Text = "Play again!";
+                score -= 1;
             }
 
-
-            // if score has reached 0 display "lose" message and set button enabled property to false
-            if (scoreDisplay.Text == "0")
+            /// GAME OVER CHECK -------------------------------------------------------
+            /// If score has reached 0, display "lose" message and disable spin button
+            /// -----------------------------------------------------------------------
+            if (score <= 0)
             {
-                outputLabel.Text = "Lose!";
+                outputLabel.Text = "You lose!";
                 spinButton.Enabled = false;
             }
 
             // display updated score
-            scoreDisplay.Text = $"{score}";
+            scoreDisplay.Text = score.ToString();
 
+            // re-enable spin button if game is still active
+            spinButton.Enabled = true;
+        }
+
+        /// IMAGE SETTER -------------------------------------------------------------
+        /// Sets the appropriate image to the BackgroundImage property of a PictureBox
+        /// based on the reel value. Uses images from Properties.Resources.
+        /// ---------------------------------------------------------------------------
+        private void SetReelImage(PictureBox box, int value)
+        {
+            switch (value)
+            {
+                case 1:
+                    box.BackgroundImage = Properties.Resources.cherry_100x125;
+                    break;
+                case 2:
+                    box.BackgroundImage = Properties.Resources.diamond_100x125;
+                    break;
+                case 3:
+                    box.BackgroundImage = Properties.Resources._7_100x125;
+                    break;
+            }
+
+            // ensure image stretches to fit PictureBox
+            box.BackgroundImageLayout = ImageLayout.Stretch;
         }
     }
 }
